e9aa4f06e6bf65f04cda71bb9c38e717e9144630
christian <christian@ubuntu>
2016-12-09 00:51:11 +0100

    asdasdad

--- TCPClient/simple_message_client.c
+++ TCPClient/simple_message_client.c
@@ -274,17 +274,19 @@ int main(int argc, const char * argv[]) {
         	   bytesread_sum+=bytesread;
         	   fprintf(stdout,"%d bytes read!\n", (int)bytesread_sum);
 
-        	   while ((char_written=fwrite(readbuffer, sizeof(char), strlen(readbuffer),write_fp))!=0){
-        	          	char_written_sum+=char_written;
+        	   while (char_written_sum!=strlen(readbuffer)){
+        			    char_written=fwrite(readbuffer, sizeof(char), strlen(readbuffer),write_fp);
+        			    if ((char_written==0)&&(ferror(write_fp))){
+        			            	      		                   fprintf(stderr,"fwrite failed!\n");
+        			            	       		                   fclose(write_fp);
+        			            	       		                   close (socketdescriptor);
+        			            	       		                   exit(EXIT_FAILURE);
+        			            	              	           	    }
+
+        			    char_written_sum+=char_written;
         	            fprintf(stdout,"%d characters written!\n", (int)char_written);
         	   }
 
-        	   if ((char_written==0)&&(ferror(write_fp))){
-        	      		                   fprintf(stderr,"fwrite failed!\n");
-        	       		                   fclose(write_fp);
-        	       		                   close (socketdescriptor);
-        	       		                   exit(EXIT_FAILURE);
-        	              	           	    }
 
            }
 
