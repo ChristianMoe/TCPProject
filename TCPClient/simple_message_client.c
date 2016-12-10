188cd78061cfdb299cdbf561732267f84d99f823
christian <christian@ubuntu>
2016-12-10 23:30:36 +0100

    commit

--- TCPClient/simple_message_client.c
+++ TCPClient/simple_message_client.c
@@ -45,7 +45,7 @@ int writefile(char *bufferstart, char *filename, int filelength, int verbose);
 /*
  * -------------------------------------------------------------- defines --
  */
-#define MAX_BUF_SIZE 1080000000 // 73741824
+#define MAX_BUF_SIZE 1000000 // 73741824
 #define READ_BUF_SIZE 5000
 
 /*
