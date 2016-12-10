492cb4a1d7e750abc0a6c54598bf524706d40e40
christian <christian@ubuntu>
2016-12-10 01:50:38 +0100

    ycyxcyx

--- TCPClient/simple_message_client.c
+++ TCPClient/simple_message_client.c
@@ -302,7 +302,7 @@ int main(int argc, const char * argv[]) {
            pos_end=strstr(pos_file,"\n");
            char* length = malloc ((int)pos_end-(int)pos_file+1);
            strncpy(length,pos_file,((int)pos_end-(int)pos_file));
-           char **endptr;
+           char **endptr=malloc ((int)pos_end-(int)pos_file+1);
            long int filelength=strtol(length, endptr, 10);
            free(length);
            fprintf(stdout,"length: %d\n",(int)filelength);
