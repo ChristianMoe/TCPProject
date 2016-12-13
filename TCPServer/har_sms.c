0fd53cca67bdce095252e1072c5c6e7b53f49532
Harald Partmann <harald.partmann@aon.at>
2016-12-14 00:14:15 +0100

    structs work

--- TCPServer/har_sms.c
+++ TCPServer/har_sms.c
@@ -30,21 +30,55 @@
 int main(int argc, char *argv[])
 {
     int sock_fd, conn_fd;
-    struct sockaddr_un my_addr, peer_addr;
+ //   struct sockaddr_un my_addr, peer_addr;
+
     socklen_t peer_addr_size;
 
-    sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+/*    // or call getaddrinfo() to fill out the struct addrinfo
+    // but you need to peer inside the structs to get the values
+    struct addrinfo {
+    	int ai_family;
+    	int ai_socktype;
+    	int ai_protocol;
+    	// struct sockaddr *ai_addr; // struct sockaddr_in
+    };
+   */
+    struct addrinfo hints; /* struct for parameters for getaddrinfo*/
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+
+    // bind the IP of the host it is running on (Beef p. 19)
+    //hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+  /*  // in for Internet - IPv4 only ! - (sockaddr_in6 for IPv6, or sockaddr as general)
+    struct sockaddr_in {
+    	short int sin_family; // Address family, AF_INET
+    	unsigned short int sin_port; // Port number
+    	struct in_addr sin_addr; // Internet address
+    	unsigned char sin_zero[8]; // Same size as struct sockaddr
+    };
+*/
+    // struct sockaddr_in sa; // IPv4
+    struct sockaddr_in my_addr;
+
+  //  sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	sock_fd = socket(hints.ai_family, hints.ai_socktype, hints.ai_protocol);
     if (sock_fd == -1)
         handle_error("socket");
 
-    memset(&my_addr, 0, sizeof(struct sockaddr_un));
+ //   memset(&my_addr, 0, sizeof(struct sockaddr_un));
+    memset(&my_addr, 0, sizeof(struct sockaddr_in));
                         /* Clear structure */
-    my_addr.sun_family = AF_UNIX;
-    strncpy(my_addr.sun_path, MY_SOCK_PATH,
-            sizeof(my_addr.sun_path) - 1);
 
-    if (bind(sock_fd, (struct sockaddr *) &my_addr,
-            sizeof(struct sockaddr_un)) == -1)
+ //   my_addr.sun_family = AF_UNIX;
+//    my_addr.sa_family = AF_INET;
+    my_addr.sin_family = AF_INET;
+ //   strncpy(my_addr.sun_path, MY_SOCK_PATH, sizeof(my_addr.sun_path) - 1);
+   ?? strncpy(my_addr.sin_path, MY_SOCK_PATH, sizeof(my_addr.sun_path) - 1);
+
+    if (bind(sock_fd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_un)) == -1)
         handle_error("bind");
 
     if (listen(sock_fd, LISTEN_BACKLOG) == -1)
