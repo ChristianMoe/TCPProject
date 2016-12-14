bc602faa9499bc0f641c5dbc2778960e4de698ed
christian <christian@ubuntu>
2016-12-14 23:17:37 +0100

    Ready to merge commit

--- TCPClient/simple_message_client.c
+++ TCPClient/simple_message_client.c
@@ -414,7 +414,6 @@ int readingmessage(char *readbuffer, int *socketdescriptor, int verbose){
 
 		while(1){
 		/* read and parse filename */
-			fprintf(stdout,"in while...\n");
 			amountread=readtillEOL(readbuffer,socketdescriptor,verbose);
 			if (amountread==-1){
 				return -1;
