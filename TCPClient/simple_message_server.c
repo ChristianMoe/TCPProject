0b88bc0640ebf43dc97a9750f380736b8028641f
christian <christian@ubuntu>
2016-12-15 23:57:37 +0100

    test

--- TCPClient/simple_message_server.c
+++ TCPClient/simple_message_server.c
@@ -122,8 +122,8 @@ char* parsecommandline(int argc, const char * argv[]){
 	int opt=0;
 	char *tmp=NULL;
 
-	/* checking whether -h is a parameter of command line */
-			while ((opt = getopt(argc,(char **) argv, "ph:")) != -1) {
+	/* checking whether -ph is a parameter of command line */
+			while ((opt = getopt(argc,(char **) argv, "p:h")) != -1) {
 				switch (opt) {
 				case 'p':
 					//tmp=malloc(strlen(optarg));
